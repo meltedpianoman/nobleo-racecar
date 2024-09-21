ea93644d511e88e049c615b8ccebad375b5c4d96
Ivo Sieben <ivo.sieben@gmail.com>
2024-09-21 08:12:55 +0200

    On straight tracks, try to speed up

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import math
 from typing import Tuple
 from pygame import Vector2, Color
 import pygame
@@ -6,6 +7,21 @@ from ...linear_math import Transform
 import socket
 import json
 from ...track import Track
+
+
+def calculate_angle(v1: Vector2, v2: Vector2):
+    dot_product = v1.dot(v2)
+    magnitude_v1 = v1.length()
+    magnitude_v2 = v2.length()
+    if magnitude_v1 == 0 or magnitude_v2 == 0:
+        return 0
+    cos_theta = dot_product / (magnitude_v1 * magnitude_v2)
+    angle_radians = math.acos(cos_theta)
+    return math.degrees(angle_radians)
+
+
+def calculate_lenth(p1: Vector2, p2: Vector2):
+    return p1.distance_to(p2)
 
 
 class DaBullet(Bot):
@@ -26,44 +42,100 @@ class DaBullet(Bot):
 
     def __init__(self, track: Track):
         super().__init__(track)
+
+        self.CURVE_ANGLE = 15
+        self.MIN_VELOCITY = 150
+        self.MAX_VELOCITY = 300
+        self.BRAKE_DISTANCE = 200
+
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_address = ('127.0.0.1', 12345)
 
-    def __del__(self):
-        self.sock.close()
+        self.calculate_waypoints()
+        self.target_velocity = 0
+
+    def calculate_waypoints(self):
+        self.waypoints = []
+        self.nof_waypoints = len(self.track.lines)
+        for i in range(len(self.track.lines)):
+            p0 = self.track.lines[i - 1]
+            p1 = self.track.lines[i]
+            p2 = self.track.lines[(i + 1) % self.nof_waypoints]
+
+            v1 = p1 - p0
+            v2 = p2 - p1
+
+            waypoint = {
+                "angle": calculate_angle(v1, v2),
+                "length": calculate_lenth(p0, p1),
+            }
+            self.waypoints.append(waypoint)
+
+        def __del__(self):
+            self.sock.close()
 
     def send_plotjuggler_data(self):
-
         data = {
-            "x": self.position.p.x,
-            "y": self.position.p.y,
+            "waypoints": self.waypoints,
+            "status": {
+                "velocity": self.velocity.length(),
+                "angle": self.angle,
+                "x": self.position.p.x,
+                "y": self.position.p.y,
+                "next_waypoint": self.next_waypoint,
+                "length_to_next_waypoint": self.target.length(),
+                "throttle": self.throttle,
+                "steer": self.steer,
+                "target_velocity": self.target_velocity,
+            },
         }
         self.sock.sendto(json.dumps(data).encode('utf-8'), self.server_address)
 
-    def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
-        self.position = position
+    def next_segment_is_curve(self):
+        length = 0.0
+        curve = False
+        waypoint = self.next_waypoint
+        while not curve and length < self.BRAKE_DISTANCE:
+            if self.waypoints[waypoint]["angle"] > self.CURVE_ANGLE:
+                curve = True
+                break
+            length = length + self.waypoints[waypoint]["length"]
+            waypoint = (waypoint + 1) % self.nof_waypoints
+        return curve, length
 
-        target = self.track.lines[next_waypoint]
+    def determine_target_velocity(self):
+        self.target_velocity = self.MAX_VELOCITY
+        (is_curve, length_to_curve) = self.next_segment_is_curve()
+        if is_curve:
+            if self.target.length() <= (self.BRAKE_DISTANCE - length_to_curve):
+                self.target_velocity = self.MIN_VELOCITY
+
+    def compute_commands(self, next_waypoint: int, position: Transform, velocity: Vector2) -> Tuple:
+        self.next_waypoint = next_waypoint
+        self.position = position
+        self.velocity = velocity
+
+        self.target = self.track.lines[next_waypoint]
         # calculate the target in the frame of the robot
-        target = position.inverse() * target
+        self.target = position.inverse() * self.target
         # calculate the angle to the target
-        angle = target.as_polar()[1]
+        self.angle = self.target.as_polar()[1]
 
         # calculate the throttle
-        target_velocity = 150
-        if velocity.length() < target_velocity:
-            throttle = 1
+        self.determine_target_velocity()
+        if self.velocity.length() < self.target_velocity:
+            self.throttle = 1
         else:
-            throttle = -1
+            self.throttle = -1
 
         # calculate the steering
-        if angle > 0:
-            steer = 1
+        if self.angle > 0:
+            self.steer = 1
         else:
-            steer = -1
+            self.steer = -1
 
         self.send_plotjuggler_data()
-        return throttle, steer
+        return self.throttle, self.steer
 
     def draw(self, map_scaled, zoom):
         return
